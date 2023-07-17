0076f056883c64a6fe9e44d3c1c3bb3068cdaf53
Rafi <rafirasheed.tc99@gmail.com>
2023-07-17 07:58:57 +0530

    variable

--- practice-codes/booking-app/main.go
+++ practice-codes/booking-app/main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("hi \n")
+	var conferenceName = "Go conference"
+	const conferenceTickets = 50
+	var remainingTicket = 50
+
+	fmt.Printf("Welcome to %v booking app \n", conferenceName)
+	fmt.Println("Remaining Ticket :", remainingTicket, "\nAvailable ticket", conferenceTickets)
+
+	var userName string
+	var userTicket int
+	// for name
+
+	userName = "Tom"
+	userTicket = 2
+	fmt.Printf("%v booked %v tickets\n", userName, userTicket)
+	fmt.Scan(&userName)
+	fmt.Printf("Name %v", userName)
 }
